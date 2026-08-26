b5b18ca7a79aed0f7ae9fabfa8781b21ef2bbf70
Vikram Shakyawal <vikramshakyawal99@gmail.com>
2026-08-26 10:27:37 +0530

    Remove backup folders from deployment source

--- .theme-backup-2026-08-18/app/control-x9p-admin-8472/page.tsx
+++
@@ -1,9 +0,0 @@
-import AdminLogin from "../../components/AdminLogin";
-
-export default function AdminLoginPage() {
-  return (
-    <main className="min-h-screen bg-zinc-950 p-10">
-      <AdminLogin />
-    </main>
-  );
-}
